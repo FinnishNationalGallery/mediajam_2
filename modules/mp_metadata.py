c2a0f685bb6ba759407160dbb62e283abdb3371c
Kari Peiponen <kari.peiponen@fng.fi>
2025-01-06 20:11:43 +0200

    Update mp_metadata.py

--- modules/mp_metadata.py
+++ modules/mp_metadata.py
@@ -102,8 +102,8 @@ def read_lido_xml():
                 record_metadata_date_elem = root.find('.//lido:recordWrap/lido:recordInfoSet/lido:recordMetadataDate', NS)
                 record_metadata_date = record_metadata_date_elem.text if record_metadata_date_elem is not None else None
                 if record_metadata_date_elem is not None:
-                    CreatedDateISO = record_metadata_date_elem.text.replace(" ", "T")
-                    record_metadata_date_elem.text = CreatedDateISO
+                    CreatedDateISO = record_metadata_date.replace(" ", "T")
+                    record_metadata_date = CreatedDateISO
                 ###
                 lidofile.close()
                 ### 
